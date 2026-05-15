b4e0c26e5f04b36d08d1872bbba1e3643301b396
Tanmay Pawar <tanmaypawar230705@gmail.com>
2026-05-15 21:15:52 +0530

    Navbar bugFix

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.png";
 import contactImg from "../../assets/contact.png";
 import { Link } from "react-scroll";
+import Menu from "../../assets/menu.png";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo" className="logo" />
@@ -14,7 +16,7 @@ const Navbar = () => {
           to="intro"
           spy={true}
           smooth={true}
-          offset={-100}
+          offset={-50}
           duration={500}
           className="desktopMenuListItem"
         >
@@ -25,7 +27,7 @@ const Navbar = () => {
           to="about"
           spy={true}
           smooth={true}
-          offset={-100}
+          offset={-50}
           duration={500}
           className="desktopMenuListItem"
         >
@@ -36,7 +38,7 @@ const Navbar = () => {
           to="skills"
           spy={true}
           smooth={true}
-          offset={-100}
+          offset={-50}
           duration={500}
           className="desktopMenuListItem"
         >
@@ -50,6 +52,63 @@ const Navbar = () => {
         <img src={contactImg} alt="" className="desktopMenuImg" />
         Contact Me
       </button>
+
+      <img
+        src={Menu}
+        alt="Menu"
+        className="mobMenu"
+        onClick={() => setShowMenu(!showMenu)}
+      />
+      <div className="navMenu" style={{ display: showMenu ? "flex" : "none" }}>
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Skills
+        </Link>
+        <Link className="listItem" onClick={() => setShowMenu(false)}>
+          projects
+        </Link>
+        <Link className="listItem" onClick={() => setShowMenu(false)}>
+          Experience
+        </Link>
+        <Link className="listItem" onClick={() => setShowMenu(false)}>
+          Resume
+        </Link>
+        <Link className="listItem" onClick={() => setShowMenu(false)}>
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
